4057e12edcf7a874a6c91b73615f194a96116075
Tejas Tagra <u7786686@anu.edu.au>
2025-04-13 22:05:22 +1000

    Edited setup.py

--- setup.py
+++ setup.py
@@ -2,27 +2,34 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name='watermarker',
-    version='1.0.0',
+    name='aquamark',
+    version='2.0.0',
     packages=find_packages(),
     install_requires=[
         'Pillow',
+        'opencv-python',
     ],
     entry_points={
         'console_scripts': [
-            'watermarker=watermarker.__main__:main',
+            'aquamark=watermarker_tool:run_cli',
         ],
     },
     include_package_data=True,
     author='Tejas Tagra',
     author_email='tejas.tagra@anu.edu.au',
-    description='A CLI tool to watermark images in bulk using a custom transparent watermark.',
+    description='A CLI and GUI tool for watermarking images and videos using light/dark detection.',
     long_description=open('README.md').read() if os.path.exists('README.md') else '',
     long_description_content_type='text/markdown',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: Other/Proprietary License',
         'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Multimedia :: Graphics :: Editors',
+        'Topic :: Multimedia :: Video',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: MacOS X',
+        'Environment :: X11 Applications :: GTK',
     ],
     python_requires='>=3.6',
 )
